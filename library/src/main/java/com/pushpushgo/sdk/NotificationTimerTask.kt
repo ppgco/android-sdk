3071db8dde56ec99e383b9c3f5597d2127ddb5dd
Mikołaj Pich <mikolaj.pich@goodylabs.com>
2020-05-08 13:36:27 +0200

    Fix unsubscribe when user disable notifiactions from channel

--- library/src/main/java/com/pushpushgo/sdk/NotificationTimerTask.kt
+++ library/src/main/java/com/pushpushgo/sdk/NotificationTimerTask.kt
@@ -1,7 +1,9 @@
 package com.pushpushgo.sdk
 
 import android.app.Application
+import android.os.Build
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.NotificationManagerCompat.IMPORTANCE_NONE
 import com.pushpushgo.sdk.network.SharedPreferencesHelper
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -12,8 +14,10 @@ internal class NotificationTimerTask(private val application: Application) : Tim
 
     private val pref = SharedPreferencesHelper(application)
 
+    private val notificationManager = NotificationManagerCompat.from(application)
+
     override fun run() {
-        if (NotificationManagerCompat.from(application).areNotificationsEnabled() && pref.isSubscribed) {
+        if (areNotificationsEnabled() && pref.isSubscribed) {
             Timber.tag(PushPushGo.TAG).d("Notifications enabled")
 
             val subscriberId = pref.subscriberId
@@ -30,5 +34,18 @@ internal class NotificationTimerTask(private val application: Application) : Tim
                 cancel()
             }
         }
+    }
+
+    private fun areNotificationsEnabled(): Boolean {
+        if (notificationManager.areNotificationsEnabled()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                return notificationManager.getNotificationChannel(application.getString(R.string.notification_channel_id)).let {
+                    it?.importance != IMPORTANCE_NONE
+                }
+            }
+            return true
+        }
+
+        return false
     }
 }
