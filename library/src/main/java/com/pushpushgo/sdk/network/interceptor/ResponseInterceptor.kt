a684976ed60cb38298a26cd99ac86baf7d2af319
Mikołaj Pich <mikolaj.pich@goodylabs.com>
2020-05-25 11:28:17 +0200

    Catch runtime exceptions in response interceptor

--- library/src/main/java/com/pushpushgo/sdk/network/interceptor/ResponseInterceptor.kt
+++ library/src/main/java/com/pushpushgo/sdk/network/interceptor/ResponseInterceptor.kt
@@ -5,7 +5,9 @@ import com.google.gson.stream.JsonReader
 import com.pushpushgo.sdk.exception.PushPushException
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
 import java.io.StringReader
+import java.lang.RuntimeException
 
 internal class ResponseInterceptor : Interceptor {
 
@@ -13,11 +15,15 @@ internal class ResponseInterceptor : Interceptor {
         val response = chain.proceed(chain.request())
         if (!response.isSuccessful) {
             val responseBodyCopy = response.peekBody(java.lang.Long.MAX_VALUE).string()
-            val reader = JsonReader(StringReader(responseBodyCopy)).apply {
-                isLenient = true
-            }
-            JsonParser.parseReader(reader).asJsonObject.get("message")?.asString?.let {
-                throw PushPushException(it)
+            try {
+                val reader = JsonReader(StringReader(responseBodyCopy)).apply {
+                    isLenient = true
+                }
+                JsonParser.parseReader(reader).asJsonObject.get("message")?.asString?.let {
+                    throw PushPushException(it)
+                }
+            } catch (e: RuntimeException) {
+                Timber.e(e)
             }
         }
         return response
