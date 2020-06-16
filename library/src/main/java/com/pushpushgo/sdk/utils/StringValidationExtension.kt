651275f30f3ca53ff85d700d523ce5b30f1a7373
Mikołaj Pich <mikolaj.pich@goodylabs.com>
2020-06-16 11:05:25 +0200

    Improve invalid project id/api key messages

--- library/src/main/java/com/pushpushgo/sdk/utils/StringValidationExtension.kt
+++ library/src/main/java/com/pushpushgo/sdk/utils/StringValidationExtension.kt
@@ -5,11 +5,11 @@ import com.pushpushgo.sdk.exception.PushPushException
 fun String.withApiKeyValidation(): String {
     if ("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$".toRegex().matches(this))
         return this
-    else throw PushPushException("Invalid API key! $this")
+    else throw PushPushException("Invalid API key! Current API key: `$this`")
 }
 
 fun String.withProjectIdValidation(): String {
     if ("[a-z0-9]{24}".toRegex().matches(this))
         return this
-    else throw PushPushException("Invalid project ID! $this")
+    else throw PushPushException("Invalid project ID! Current project ID: `$this`")
 }
