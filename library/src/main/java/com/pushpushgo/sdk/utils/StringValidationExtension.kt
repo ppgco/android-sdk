3acf1faab14b52c653a84c23982b50c38d25f7fd
Mikołaj Pich <mikolaj.pich@goodylabs.com>
2020-06-18 17:50:22 +0200

    Add internal modifier to validate methods

--- library/src/main/java/com/pushpushgo/sdk/utils/StringValidationExtension.kt
+++ library/src/main/java/com/pushpushgo/sdk/utils/StringValidationExtension.kt
@@ -2,13 +2,13 @@ package com.pushpushgo.sdk.utils
 
 import com.pushpushgo.sdk.exception.PushPushException
 
-fun validateApiKey(apiKey: String) {
+internal fun validateApiKey(apiKey: String) {
     if (!"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$".toRegex().matches(apiKey)) {
         throw PushPushException("Invalid API key! Current API key: `$apiKey`")
     }
 }
 
-fun validateProjectId(projectId: String) {
+internal fun validateProjectId(projectId: String) {
     if (!"[a-z0-9]{24}".toRegex().matches(projectId)) {
         throw PushPushException("Invalid project ID! Current project ID: `$projectId`")
     }
