a2a75b4cddd5042d0f8ce491442a3bc16feaff4e
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-08-29 19:08:19 +0200

    feat(PushPushGo): add to instance custom flags declaration for launcher activity intent

--- sample/src/main/java/com/pushpushgo/sample/MainApplication.kt
+++ sample/src/main/java/com/pushpushgo/sample/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.pushpushgo.sample
 
 import android.app.Application
+import android.content.Intent
 import com.pushpushgo.sdk.PushPushGo
 import timber.log.Timber
 
@@ -9,6 +10,8 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        PushPushGo.getInstance(this)
+        PushPushGo
+            .getInstance(this)
+            .setCustomClickIntentFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
     }
 }
